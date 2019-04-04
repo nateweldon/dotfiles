5d6edc7b9f0bc755b05940f319648e9ba6882b0b
Jack Venuti <jvenuti@paychex.com>
2019-04-04 13:26:44 -0400

    Change c:repo to use workspace variable; update cd commands/aliases; add additional aliases/functions

--- psModules/paychex/flex/flex_dirMoves.psm1
+++ psModules/paychex/flex/flex_dirMoves.psm1
@@ -1,76 +1,110 @@
 ################################################################################################
-function wlflex {  
+function cd_weblogic {
 <#
 .SYNOPSIS
  Move to Weblogic directory for flex
 .DESCRIPTION
-cd C:/Oracle/Middleware_122100/user_projects/domains/flex
+Set-Location C:/Oracle/Middleware_122100/user_projects/domains/flex
 #>
-	cd C:/Oracle/Middleware_122100/user_projects/domains/flex
+	Set-Location C:/Oracle/Middleware_122100/user_projects/domains/flex
 }
+Set-Alias cd-weblogic cd_weblogic
+Set-Alias cd-wlflex cd_weblogic
+Set-Alias wlflex cd_weblogic
 
 ################################################################################################
-function wl11flex {
+function cd_weblogic_11 {
     <#
 .SYNOPSIS
 Moves to Weblogic 11 for png
 .DESCRIPTION
-cd C:/Oracle/Middleware/user_projects/domains/png
+Set-Location C:/Oracle/Middleware/user_projects/domains/png
 #>
-    cd C:/Oracle/Middleware/user_projects/domains/png
+    Set-Location C:/Oracle/Middleware/user_projects/domains/png
 }
+Set-Alias cd-wl-11 cd_weblogic_11
+Set-Alias cd-wl11 cd_weblogic_11
+Set-Alias wl11flex cd_weblogic_11
 
 ################################################################################################
-function flex {
+function cd_develop {
     <#
 .SYNOPSIS
 Moves to top level flex directory
 .DESCRIPTION
-cd C:/repo/develop/
+Set-Location $env:WORKSPACE/develop/
 #>
-    cd C:/repo/develop/
+    Set-Location $env:WORKSPACE/develop/
 }
+Set-Alias cd-develop cd_develop
+Set-Alias cd-flex cd_develop
+Set-Alias flex cd_develop
 
 ################################################################################################
-function remote {
+function cd_remote {
     <#
 .SYNOPSIS
 Moves to top level remote
 .DESCRIPTION
-cd C:/repo/remote/
+Set-Location $env:WORKSPACE/remote/
 #>
-    cd C:/repo/remote/
+    Set-Location $env:WORKSPACE/remote/
 }
+Set-Alias cd-remote cd_remote
+Set-Alias remote cd_remote
 
 ################################################################################################
-function apayroll {
+function cd_app_payroll {
   <#git 
 .SYNOPSIS
 Moves to flex app_payroll directory
 .DESCRIPTION
-cd C:/repo/develop/app_payroll/
+Set-Location $env:WORKSPACE/develop/app_payroll/
 #>
-    cd C:/repo/develop/app_payroll/
+    Set-Location $env:WORKSPACE/develop/app_payroll/
 }
+Set-Alias cd-payroll cd_app_payroll
+Set-Alias cdp cd_app_payroll
+Set-Alias apayroll cd_app_payroll
 
 ################################################################################################
-function alanding {
+function cd_app_landing {
   <#
 .SYNOPSIS
 Move to flex app_landing directory
 .DESCRIPTION
-cd C:/repo/develop/app_landing/
+Set-Location $env:WORKSPACE/develop/app_landing/
 #>
-    cd C:/repo/develop/app_landing/
+    Set-Location $env:WORKSPACE/develop/app_landing/
 }
+Set-Alias cd-landing cd_app_landing
+Set-Alias cdl cd_app_landing
+Set-Alias alanding cd_app_landing
 
 ################################################################################################
-function acommon {
+function cd_app_common {
   <#
 .SYNOPSIS
 Move to flex app_common directory
 .DESCRIPTION
-cd C:/repo/develop/app_common/
+Set-Location $env:WORKSPACE/develop/app_common/
 #>
-    cd C:/repo/develop/app_common/
+    Set-Location $env:WORKSPACE/develop/app_common/
 }
+Set-Alias cd-common cd_app_common
+Set-Alias cdc cd_app_common
+Set-Alias acommon cd_app_common
+
+################################################################################################
+function cd_app_docmanagement {
+    <#
+.SYNOPSIS
+Move to flex app_docmanagement directory
+.DESCRIPTION
+Set-Location $env:WORKSPACE/develop/app_docmanagement/
+#>
+    Set-Location $env:WORKSPACE/develop/app_docmanagement/
+}
+Set-Alias cd-docmanagement cd_app_docmanagement
+Set-Alias cd-appdoc cd_app_docmanagement
+Set-Alias cd-doc cd_app_docmanagement
