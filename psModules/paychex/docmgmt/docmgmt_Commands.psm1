3e2c35d44c32f5483614001a7ab8baafd4e3ec6e
Jack Venuti <jvenuti@paychex.com>
2019-04-10 14:47:34 -0400

    Add commands for building and running docman alfresco svc

--- psModules/paychex/docmgmt/docmgmt_Commands.psm1
+++ psModules/paychex/docmgmt/docmgmt_Commands.psm1
@@ -89,8 +89,44 @@ function start_docman_svc {
 Set-Alias start-ds start_docman_svc
 
 function start_docman_alfresco_svc {
+<#
+.SYNOPSIS
+ Start the Docman Alfresco Svc
+.DESCRIPTION
+Set-Location $env:REPO/remotes/docmanagement-alfresco-svc/runner/docker-compose
+docker-compose up
+#>
     Set-PSTitle("Running DocMan Alfresco Svc")
     cd_docmanagement_alfresco_svc
-    ./gradlew bootRun
+    Set-Location runner/docker-compose
+    docker-compose up
 }
 Set-Alias start-das start_docman_alfresco_svc
+Set-Alias run-das start_docman_alfresco_svc
+
+function build_docman_alfresco_svc {
+<#
+.SYNOPSIS
+ Stop/remove existing docker images and build new images for the Docman Alfresco Svc
+.DESCRIPTION
+Set-Location $env:REPO/remotes/docmanagement-alfresco-svc
+docker container <stop/rm> docker-compose_<content/share/oracle>_1
+./gradlew clean build dockerBuild
+#>
+    cd_docmanagement_alfresco_svc
+
+    # Make sure there are no existing Docker containers, that would
+    # prohibit the associated Docker images from being removed and updated.
+    docker container stop docker-compose_content_1
+    docker container rm docker-compose_content_1
+
+    docker container stop docker-compose_share_1
+    docker container rm docker-compose_share_1
+
+    docker container stop docker-compose_oracle_1
+    docker container rm docker-compose_oracle_1
+
+    ./gradlew clean build dockerBuild
+}
+Set-Alias build-das build_docman_alfresco_svc
+Set-Alias build-all-modules build_docman_alfresco_svc
